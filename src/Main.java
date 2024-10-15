54659b94d5ab9436c89ce27dbf95b09582676053
Sarthak Pramod Mhatre <2023.sarthak.mhatre@ves.ac.in>
2024-10-15 18:30:48 +0530

    Initial commit for Jenkins with Ant

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello I am Sarthak Pramod Mhatre from D10B 33,\nI am testing Jenkins with Ant!");
+    }
+}
